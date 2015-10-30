d77d9e3e3e436f2c7f264703113e4b3cd54d4592
abell25 <bellar@uwm.edu>
2015-10-30 14:06:31 -0500

    checking in model library

--- model_library.py
+++ model_library.py
@@ -1,14 +1,15 @@
 __author__ = 'anthony bell'
 
+
+import numpy as np
+import random
+
+
 #small regression
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, ExtraTreesRegressor
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.svm import SVR
 
-try:
-    import xgboost as xgb
-except Exception, e:
-    print('failed loading xgb module!: {0}'.format(e))
 
 #small classification
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, ExtraTreesClassifier
@@ -33,36 +34,155 @@ class ModelLibrary():
 
     model_libraries = {
         'small regression': {
-	    'xgb': {
-	        'model': xgb.XGBRegression,
-		'parameter_ranges': {
-                    'max_depth': range(1, 9),
-	        }
-	    }
-	}
+            'GradientBoostingRegressor': {
+                'model': GradientBoostingRegressor,
+                'parameter_ranges': {
+                    'loss': ['ls'],
+                    'learning_rate': np.logspace(-3, 0, 1000),
+                    'n_estimators': np.linspace(1, 2000, 1000, dtype=int),
+                    'max_depth': np.linspace(1, 12, 12, dtype=int),
+                    'subsample': np.linspace(0.5, 1.0, 50)
+
+                }
+            },
+            'RandomForestRegressor': {
+                'model': RandomForestRegressor,
+                'parameter_ranges': {
+                    'max_depth': [None] + list(np.linspace(1, 80, 80, dtype=int)),
+                    'n_estimators': np.linspace(1, 1000, 1000, dtype=int),
+                    'min_samples_split': range(2, 21),
+                    'max_features': ['auto', 'sqrt', 'log2']
+                }
+            },
+            'KNeighborsRegressor': {
+                'model': KNeighborsRegressor,
+                'parameter_ranges': {
+                    'n_neighbors': range(1, 61),
+                    'weights': ['uniform', 'distance'],
+                    'p': [1,2],
+                    'algorithm': ['ball_tree', 'kd_tree', 'auto'],
+                    'leaf_size': range(10,51)
+                }
+            }
+
+	    },
+        'large regression': {
+            'LinearRegression': {
+                'model': LinearRegression,
+                'parameter_ranges': {
+                    'fit_intercept': [True],
+                    'normalize': [False]
+                }
+            },
+            'LinearSVR': {
+                'model': LinearSVR,
+                'parameter_ranges': {
+                    'C': np.logspace(-3, 3, 1000),
+                    'fit_intercept': [True]
+                }
+            },
+            'SGDRegressor': {
+                'model': SGDRegressor,
+                'parameter_ranges': {
+                    'penalty': ['l1', 'l2'],
+                    'alpha': np.logspace(-6, -1, 1000),
+                    'l1_ratio': np.logspace(-3, -0.5, 1000),
+                    'loss': ['squared_loss']
+                }
+            }
+        },
+        'small classification': {
+            'GradientBoostingClassifier': {
+                'model': GradientBoostingClassifier,
+                'parameter_ranges': {
+                    'loss': ['deviance'],
+                    'learning_rate': np.logspace(-3, 0, 1000),
+                    'n_estimators': np.linspace(1, 2000, 1000, dtype=int),
+                    'max_depth': np.linspace(1, 12, 12, dtype=int),
+                    'subsample': np.linspace(0.5, 1.0, 50)
+
+                }
+            },
+            'RandomForestClassifier': {
+                'model': RandomForestClassifier,
+                'parameter_ranges': {
+                    'max_depth': [None] + list(np.linspace(1, 80, 80, dtype=int)),
+                    'n_estimators': np.linspace(1, 1000, 1000, dtype=int),
+                    'min_samples_split': range(2, 21),
+                    'max_features': ['auto', 'sqrt', 'log2']
+                }
+            },
+            'KNeighborsClassifier': {
+                'model': KNeighborsClassifier,
+                'parameter_ranges': {
+                    'n_neighbors': range(1, 61),
+                    'weights': ['uniform', 'distance'],
+                    'p': [1,2],
+                    'algorithm': ['ball_tree', 'kd_tree', 'auto'],
+                    'leaf_size': range(10,51)
+                }
+            },
+            'LogisticRegression': {
+                'model': LogisticRegression,
+                'parameter_ranges': {
+                    'penalty':['l1', 'l2'],
+                    'C':np.logspace(-2, 2, 1000),
+                    'class_weight':[None],
+                    'fit_intercept':[True]
+                }
+            }
+        },
+        'large classification': {
+            'LogisticRegression': {
+                'model': LogisticRegression,
+                'parameter_ranges': {
+                    'penalty':['l1', 'l2'],
+                    'C':np.logspace(-2, 2, 1000),
+                    'class_weight':[None],
+                    'fit_intercept':[True]
+                }
+            },
+            'SGDClassifier': {
+                'model': SGDClassifier,
+                'parameter_ranges': {
+                    'penalty': ['l1', 'l2'],
+                    'alpha': np.logspace(-6, -1, 1000),
+                    'l1_ratio': np.logspace(-3, -0.5, 1000),
+                    'loss': ['hinge']
+                }
+            }
+        }
     }
 
     
-    def __init__(self, modelType):
-       self.modelType = modelType
+    def __init__(self, task_type, problem_size):
+        self.task_type = task_type
+        self.problem_size = problem_size
+        self.model_library = self.model_libraries['{0} {1}'.format(problem_size, task_type)]
 
-    def getModelLibrary(self):
-    #todo: get model from library
-	pass
+    def generateModel(self, model_selection_method='random', parameter_selection_method='random'):
+        model_name = self.pickModel(model_selection_method)
+        uninitialized_model = self.model_library[model_name]
+
+        ModelClass, parameter_ranges = uninitialized_model['model'], uninitialized_model['parameter_ranges']
+        chosen_parameters = self.pickParameters(parameter_ranges, parameter_selection_method)
+        model = ModelClass(**chosen_parameters)
+        return model
 
 
+    def pickModel(self, model_selection_method):
+        if model_selection_method is 'random':
+            model_name = random.choice(self.model_library.keys())
+            return model_name
+        else:
+            raise Exception('selectionMethod "{0}" is not a valid model selection method!'.format(model_selection_method))
 
-class Model():
-    def __init__(self, name, model, parameter_ranges):
-        self.name = name
-    #self.model = model
-    #self.parameter_ranges = parameter_ranges
-    #self.scores = []
+    def pickParameters(self, parameter_ranges, parameter_selection_method):
+        chosen_parameter_values = {}
+        for param in parameter_ranges:
+            if parameter_selection_method is 'random':
+                chosen_parameter_values[param] = random.choice(parameter_ranges[param])
+            else:
+                raise Exception('selectionMethod {0} is not a valid parameter selection method')
 
-    def initModel(self, parameters=None):
-        pass
-        #model = self.model()
-	    #TODO: set parameter valiues for model
-	
-    
-    
+        return chosen_parameter_values
---
+++ test_files/test_modelLibrary.py
@@ -0,0 +1,32 @@
+from unittest import TestCase
+from model_library import ModelLibrary
+
+__author__ = 'anthony bell'
+
+
+class TestModelLibrary(TestCase):
+  def setUp(self):
+    pass
+
+  def test_generateModel(self):
+    self.fail()
+
+  def test_pickModel(self):
+    model_library = ModelLibrary('classification', 'small')
+    available_model_names = model_library.model_libraries['small classification'].keys()
+
+    model_name = model_library.pickModel(model_selection_method='random')
+
+    self.assertTrue(model_name in available_model_names)
+
+
+  def test_pickParameters(self):
+    model_library = ModelLibrary('classification', 'small')
+    model_name = model_library.model_libraries['small classification'].keys()[0]
+    uninitialized_model = model_library.model_libraries['small classification'][model_name]
+    ModelClass, parameter_ranges = uninitialized_model['model'], uninitialized_model['parameter_ranges']
+
+    chosen_parameters = model_library.pickParameters(parameter_ranges, parameter_selection_method='random')
+
+    for param in parameter_ranges:
+      self.assertTrue(chosen_parameters[param] in parameter_ranges[param])
